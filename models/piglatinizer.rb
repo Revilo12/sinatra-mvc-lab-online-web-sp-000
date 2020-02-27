7293cf8d1f9c048e2cda9bbfbe5eb80cf2aea204
Oliver Dahl <oliverpdahl@gmail.com>
2020-02-27 01:06:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,7 +13,7 @@ class PigLatinizer
   end
 
   def consonants_from_front(word)
-    if !@@vowels.include?(word[0])
+    if !@@vowels.include?(word[0]) && word
       word[0] + consonants_from_front(word[1..-1])
     end
   end
